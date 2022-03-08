7a1c7dbb86806a34487d2dcbda78f7fc9d9c3439
DisableGraphics <48135147+DisableGraphics@users.noreply.github.com>
2022-03-08 19:56:02 +0100

    Update webPageDownloader.c

--- webPageDownloader.c
+++ webPageDownloader.c
@@ -4,21 +4,17 @@
 /*English is not my main language. If you notice something wrong, please modify it. It's everyone's code ;)*/
 
 char url[1024];
-char command[1043];
-char hyperlinks[3];
+char command[1111];
 
 int main(void){
     /*The url to be downloaded and the number of web pages directly related to be downloaded*/
 	printf("Please enter the name of the webpage you want to download\n");
 	scanf("%s\n", url);
-	printf("Enter the number of web pages directly related to the web page to be downloaded (enter a number between 0 and 999. if you want all the related web pages, type 'inf' without the quotes\n");
 	/*Sorry for this last line, Geany IDE doesn't like to printf in two lines*/    
-	scanf("%s\n", hyperlinks);
 
     /*Concatenate the strings*/
-	strcpy(command, "wget -r -l ");
-        strcat(command, hyperlinks);
-        strcat(command, " ");
+	/*Now it will download better than before*/
+	strcpy(command, "wget -m -F -p -np -k -erobots=off -U mozilla --limit-rate=50K --wait=2 --html-extension ");
         strcat(command, url);
 
 	system(command);
