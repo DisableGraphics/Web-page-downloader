3ea8c8da867c87f7581e17fd42e2a4f3d6193a3e
DisableGraphics <elchifladod@gmail.com>
2022-08-28 07:32:34 +0200

    Added: Arguments, removed the double line bug

--- webPageDownloader.c
+++ webPageDownloader.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /*English is not my main language. If you notice something wrong, please modify it. It's everyone's code ;)*/
 
 char url[1024];
 char command[1111];
 
-int main(void){
-    /*The url to be downloaded and the number of web pages directly related to be downloaded*/
-	printf("Please enter the name of the webpage you want to download\n");
-	scanf("%s\n", url);
-	/*Sorry for this last line, Geany IDE doesn't like to printf in two lines*/    
+int main(int argc, char** argv)
+{
+	if(argc > 1){ 	
+		/*If there are arguments, no need to scanf the web page to download*/
+		strcpy(command, "wget -m -F -p -np -k -erobots=off -U mozilla --limit-rate=50K --wait=2 --html-extension ");
+		strcpy(argv[1], url);
+		strcat(command, url);
+	}
+	else{
+		/*The url to be downloaded and the number of web pages directly related to be downloaded*/
+		printf("Please enter the name of the webpage you want to download\n");
+		scanf("%s", url);
 
-    /*Concatenate the strings*/
-	/*Now it will download better than before*/
-	strcpy(command, "wget -m -F -p -np -k -erobots=off -U mozilla --limit-rate=50K --wait=2 --html-extension ");
-        strcat(command, url);
+		
+		/*Concatenate the strings*/
+		strcpy(command, "wget -m -F -p -np -k -erobots=off -U mozilla --limit-rate=50K --wait=2 --html-extension ");/*Now it will download better than before, since it had a rather basic command*/
+		strcat(command, url);
+	}
 
-	system(command);
+	system(command); /*Execute the command to download the page*/
 }
