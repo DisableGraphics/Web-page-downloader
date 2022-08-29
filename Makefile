79cd1462ed43508b85a538eb494cdbde21c413ac
DisableGraphics <elchifladod@gmail.com>
2022-08-29 19:36:50 +0200

    Wouldn't download a web page passed as an argument

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 all:
 	gcc webPageDownloader.c -o webpagedownloader
+linux-32:
+	gcc webPageDownloader.c -o webpagedownloader -m32
--- webPageDownloader.c
+++ webPageDownloader.c
@@ -4,24 +4,22 @@
 
 /*English is not my main language. If you notice something wrong, please modify it. It's everyone's code ;)*/
 
-
-
-int main(int argc, char** argv)
-{
-	char url[1024];
+int main(int argc, char** argv){
+	
 	char command[1111];
 	if(argc > 1){ 	
 		/*If there are arguments, no need to scanf the web page to download*/
 		strcpy(command, "wget -m -F -p -np -k -erobots=off -U mozilla --limit-rate=50K --wait=2 --html-extension ");
-		strcpy(argv[1], url);
-		strcat(command, url);
+		
+		strcat(command, argv[1]);
+		printf("%s\n", command);
 	}
 	else{
+		char url[1024];
 		/*The url to be downloaded and the number of web pages directly related to be downloaded*/
 		printf("Please enter the name of the webpage you want to download\n");
 		scanf("%s", url);
 
-		
 		/*Concatenate the strings*/
 		strcpy(command, "wget -m -F -p -np -k -erobots=off -U mozilla --limit-rate=50K --wait=2 --html-extension ");/*Now it will download better than before, since it had a rather basic command*/
 		strcat(command, url);
